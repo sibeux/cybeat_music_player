5fbbbd50c6793ffe454dd826e9349d39d71df8b9
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-05-14 17:54:50 +0700

    Refactor URL formatting to handle raw GitHub links correctly

--- lib/components/url_formatter.dart
+++ lib/components/url_formatter.dart
@@ -13,14 +13,13 @@ String regexGdriveLink(
     final regExp = RegExp(r'files\/([a-zA-Z0-9_-]+)\?');
     final match = regExp.firstMatch(url);
     return "https://www.googleapis.com/drive/v3/files/${match!.group(1)}?alt=media&key=$key";
-  } else if (url.contains('https://github.com/')) {
-    String rawUrl = url
+  } else if (url.contains('https://github.com/') && url.contains('raw=true')) {
+    return url
         .replaceFirst(
             "https://github.com/", "https://raw.githubusercontent.com/")
         .replaceFirst("/blob/", "/refs/heads/")
         .split('?')
         .first; // menghapus query string
-    return rawUrl;
   } else {
     return url;
   }
