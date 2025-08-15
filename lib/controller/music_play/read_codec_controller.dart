23f73c22fafa867cdec7a8950e839e4459091abb
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-08-16 02:16:26 +0700

    add ffmpeg kit depedency and upgrade minsdkversion from 21 to 24

--- lib/controller/music_play/read_codec_controller.dart
+++ lib/controller/music_play/read_codec_controller.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import 'package:cybeat_music_player/components/colorize_terminal.dart';
+import 'package:ffmpeg_kit_flutter_new/ffprobe_kit.dart';
+import 'package:ffmpeg_kit_flutter_new/stream_information.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
 
 class ReadCodecController extends GetxController {
   var sampleRate = '--'.obs;
@@ -14,36 +13,63 @@ class ReadCodecController extends GetxController {
     bitsPerRawSample.value = '--';
     bitRate.value = '--';
 
-    const uri =
-        "https://sibeux.my.id/cloud-music-player/database/mobile-music-player/api/read_codec.php";
+    // const uri =
+    //     "https://sibeux.my.id/cloud-music-player/database/mobile-music-player/api/read_codec.php";
 
     try {
-      final response = await http.post(
-        Uri.parse(uri),
-        body: {
-          'url': url,
-        },
-      );
+      // 1. Jalankan FFprobe langsung dengan URL
+      final session = await FFprobeKit.getMediaInformation(url);
+      final information = session.getMediaInformation();
 
-      if (response.body.isEmpty) {
-        logError('Error onReadCodec: Response body is empty');
-        return;
+      if (information != null) {
+        // 2. Proses ekstraksi data sama persis seperti file lokal
+        final audioStream = information.getStreams().firstWhere(
+              (s) => s.getType() == 'audio',
+              orElse: () => StreamInformation(information.getAllProperties()),
+            );
+
+        bitsPerRawSample.value =
+            audioStream.getProperty('bits_per_raw_sample') ?? '--';
+        final String fileSampleRate =
+            audioStream.getProperty('sample_rate') ?? '--';
+        final String fileBitRate = audioStream.getProperty('bit_rate') ?? '--';
+
+        sampleRate.value = fileSampleRate != '--'
+            ? (int.parse(fileSampleRate) / 1000).toString()
+            : '--';
+        bitRate.value = fileBitRate != '--'
+            ? (int.parse(fileBitRate) / 1000).toStringAsFixed(0)
+            : '--';
+
+        logSuccess(audioStream.getAllProperties().toString());
       }
 
-      final responseBody = jsonDecode(response.body);
+      // final response = await http.post(
+      //   Uri.parse(uri),
+      //   body: {
+      //     'url': url,
+      //   },
+      // );
 
-      bitsPerRawSample.value =
-          responseBody["streams"][0]["bits_per_raw_sample"] ?? '--';
-      final String fileSampleRate =
-          responseBody["streams"][0]["sample_rate"] ?? '--';
-      final String fileBitRate = responseBody["format"]["bit_rate"] ?? '--';
+      // if (response.body.isEmpty) {
+      //   logError('Error onReadCodec: Response body is empty');
+      //   return;
+      // }
 
-      sampleRate.value = fileSampleRate != '--'
-          ? (int.parse(fileSampleRate) / 1000).toString()
-          : '--';
-      bitRate.value = fileBitRate != '--'
-          ? (int.parse(fileBitRate) / 1000).toStringAsFixed(0)
-          : '--';
+      // final responseBody = jsonDecode(response.body);
+
+      // bitsPerRawSample.value =
+      //     responseBody["streams"][0]["bits_per_raw_sample"] ?? '--';
+      // final String fileSampleRate =
+      //     responseBody["streams"][0]["sample_rate"] ?? '--';
+      // final String fileBitRate = responseBody["format"]["bit_rate"] ?? '--';
+
+      // sampleRate.value = fileSampleRate != '--'
+      //     ? (int.parse(fileSampleRate) / 1000).toString()
+      //     : '--';
+      // bitRate.value = fileBitRate != '--'
+      //     ? (int.parse(fileBitRate) / 1000).toStringAsFixed(0)
+      //     : '--';
     } catch (e) {
       logError('Error onReadCodec: $e');
     }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import audio_service
 import audio_session
+import ffmpeg_kit_flutter_new
 import firebase_core
 import firebase_crashlytics
 import flutter_image_compress_macos
@@ -18,6 +19,7 @@ import sqflite_darwin
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
