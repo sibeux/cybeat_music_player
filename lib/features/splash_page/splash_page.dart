2444a24e951d6e6c07cf4e1c6667689198bfebb2
mnasrul.wahabi <mnasrul.wahabi@internship.pertamina.com>
2026-02-16 21:48:14 +0700

    feat: implement initial splash screen page

--- lib/features/splash_page/splash_page.dart
+++ lib/features/splash_page/splash_page.dart
@@ -1,6 +1,7 @@
-import 'package:cybeat_music_player/common/utils/color_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class SplashPage extends StatelessWidget {
@@ -8,15 +9,24 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Membuat status bar menjadi transparan agar warna background terlihat penuh
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // Untuk Android
+      statusBarIconBrightness: Brightness.light, // Ikon putih (karena bg gelap)
+    ));
     // Remove the native splash screen
     FlutterNativeSplash.remove();
     return Scaffold(
+      // Properti ini opsional jika tidak pakai AppBar,
+      // tapi berguna untuk memastikan konten benar-benar full.
+      extendBody: true,
+      extendBodyBehindAppBar: true,
       backgroundColor: HexColor("#282d46"),
       body: Center(
         child: Image.asset(
           'assets/images/cybeat_splash.png',
-          width: 150,
-          height: 150,
+          width: 150.w,
+          height: 150.h,
         ),
       ),
     );
